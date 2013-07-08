13945ad7e5345a04e0267493fbb384d040d2c343
Isaac Zhi <QFWFQzhi@gmail.com>
2013-07-08 08:37:48 +0000

    modify web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+
+var indexfilebuffer = fs.readFileSync('index.html');
+
+var indexstring = indexfilebuffer.toString();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(indexstring);
 });
 
 var port = process.env.PORT || 5000;
